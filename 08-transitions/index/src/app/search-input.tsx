b5a0d5c52be16446accfad8705475e2ef1506ce1
Ryan Toronto <ryanto@gmail.com>
2023-09-07 14:59:58 -0400

    end of 9

--- 08-transitions/index/src/app/search-input.tsx
+++ 08-transitions/index/src/app/search-input.tsx
@@ -2,9 +2,15 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { Spinner } from "./components/spinner";
 
 export function SearchInput({ search }: { search?: string }) {
   const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
+  const isSearching = timeoutId || isPending;
 
   return (
     <div className="relative rounded-md shadow-sm">
@@ -22,13 +28,31 @@ export function SearchInput({ search }: { search?: string }) {
         placeholder="Search"
         defaultValue={search}
         onChange={(event) => {
-          if (event.target.value) {
-            router.push(`/?search=${event.target.value}`);
-          } else {
-            router.push("/");
-          }
+          clearTimeout(timeoutId);
+
+          const id = setTimeout(() => {
+            startTransition(() => {
+              if (event.target.value) {
+                router.push(`/?search=${event.target.value}`);
+              } else {
+                router.push("/");
+              }
+
+              setTimeoutId(undefined);
+            });
+          }, 500);
+
+          setTimeoutId(id);
         }}
       />
+      {isSearching && (
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+          <Spinner
+            className="h-5 w-5 animate-spin text-gray-400"
+            aria-hidden="true"
+          />
+        </div>
+      )}
     </div>
   );
 }
